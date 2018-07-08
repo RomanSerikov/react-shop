c14689fb84318c7c5ecf62ef7704df5c3bddcc94
Roman Serikov <roman.serikov@yandex.ru>
2018-07-08 15:12:20 +0300

    Add: react hello world component

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './src/hello';
 import Arithmetic from './src/arithmetic';
 
-console.log('Hello, Parcel!');
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('root')
+);
+
 console.log(new Arithmetic().sum(40, 2));
 console.log(new Arithmetic().diff(40, 2));
 console.log(new Arithmetic().product(40, 2));
